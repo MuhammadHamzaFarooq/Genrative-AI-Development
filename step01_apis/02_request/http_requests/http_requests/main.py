888803e10aecea6c8eadb18d1740e521852af25d
muhammadhamzafarooq <mhamza2021999@gmail.com>
2024-04-06 01:05:17 +0500

    calling get api & printing  responses Like status code , json response,  text response, headers

--- step01_apis/02_request/http_requests/http_requests/main.py
+++ step01_apis/02_request/http_requests/http_requests/main.py
@@ -1,1 +1,25 @@
-print("Learn Api Call & Data Fetching With Request Package.")
+import requests
+from requests.models import Response
+from requests.structures import  CaseInsensitiveDict
+
+response : Response = requests.get("https://simple-books-api.glitch.me/status")
+print("api response: ", response)
+
+# get status for this response object
+status_code : int = response.status_code
+print("status code: ", status_code)
+
+# get json response object
+json_response : dict = response.json()
+print("json response: ", json_response,type(json_response))
+
+# get response text
+response_txt : str = response.text
+print("response text: ", response_txt, type(response_txt))
+
+# get response headers
+response_headers : CaseInsensitiveDict = response.headers
+print("response headers: ", response_headers, type(response_headers))
+
+# print all of responses like status code, json, text, headers
+print(response.status_code, response.json(), response.text, response.headers)
